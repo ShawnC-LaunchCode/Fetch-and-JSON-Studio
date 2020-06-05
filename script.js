60c2b2190d2659d845dbc6422cb03c79026a4f39
Shawn C <scooter4356@gmail.com>
2020-06-05 10:54:05 -0500

    have fetched list displaying single result many times

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 // TODO: add code here
+window.addEventListener("load",function(){
+    let url = "https://handlers.education.launchcode.org/static/astronauts.json";
+    let json=[];
+    //this.console.log("in onLoad, fetch url:", url);
+    fetch(url)
+        .then(function(response){
+            response.json()
+            .then(function(jsonTemp){
+                json=jsonTemp;
+                //console.log("json in loop:",json);
+
+                let template="";
+
+                for(let i =0; i<json.length;i++){
+                    template+=`<div class="astronaut">
+                    <div class="bio">
+                       <h3>Mae Jemison</h3>
+                       <ul>
+                          <li>Hours in space: 190</li>
+                          <li>Active: false</li>
+                          <li>Skills: Physician, Chemical Engineer</li>
+                       </ul>
+                    </div>
+                    <img class="avatar" src="images/mae-jemison.jpg">
+                 </div>`
+                }
+                 let container = document.getElementById("container");
+                 container.innerHTML=template;
+
+            })
+            
+        })
+    
+    
+    
+
+});
